47afbf67a8ba6c092ca29f72ad1bd91c187ae1a7
ek-mlg <ek.mlg@icloud.com>
2023-09-30 17:29:55 +0300

    81 add users selectors

--- src/сomponents/users/UsersContainer.tsx
+++ src/сomponents/users/UsersContainer.tsx
@@ -11,6 +11,14 @@ import {
 import Users from "./Users";
 import {CircularProgress} from "@material-ui/core";
 import {compose} from "redux";
+import {
+    getCurrentPage,
+    getFollowingInProgress,
+    getIsFetching,
+    getPageSize,
+    getTotalUsersCount,
+    getUsers
+} from "../../Redux/users-selectors";
 
 
 type MapDispatchPropsType = {
@@ -59,12 +67,12 @@ class UsersContainer extends React.Component<UsersPropsType, UsersType> {
 
 const mapStateToProps = (state: AppRootStateType): InitialStateType => {
     return {
-        users: state.users.users,
-        pageSize: state.users.pageSize,
-        totalUsersCount: state.users.totalUsersCount,
-        currentPage: state.users.currentPage,
-        isFetching: state.users.isFetching,
-        followingInProgress: state.users.followingInProgress
+        users: getUsers(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state)
     }
 }
 
