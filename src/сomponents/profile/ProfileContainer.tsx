82aef64a0dfbcc67d50614b9b8acdfc4c0631099
ek-mlg <ek.mlg@icloud.com>
2023-09-30 21:04:26 +0300

    84 nothing

--- src/сomponents/profile/ProfileContainer.tsx
+++ src/сomponents/profile/ProfileContainer.tsx
@@ -5,7 +5,6 @@ import {getStatusTC, getUserProfileTC, updateStatusTC} from "../../Redux/profile
 import {AppRootStateType} from "../../Redux/redux-store";
 import {useParams} from "react-router-dom";
 import {compose} from "redux";
-import {withAuthRedirect} from "../../HOC/withAuthRedirect";
 
 type MapStatePropsType = {
     profile: null,
